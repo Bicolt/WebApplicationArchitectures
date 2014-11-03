ef7006a30645810726c2cebc1fffb44005170cc2
Nicolas Benning <nbenning@free.fr>
2014-11-03 16:40:49 +0000

    board template

--- Board/app/templates/template_board.php
+++ Board/app/templates/template_board.php
@@ -1,14 +1,29 @@
-<!doctype html>
+<! DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta name="description" content="template MVC">
-		<meta name="author" content="Nicolas Benning">
-		<link href="" rel="stylesheet">
-		<title>Template example</title>
-	</head>
-	
-	<body>
-		<?php echo $data ?>
-	</body>
+<?php include_once 'html_doctype_and_head.php'; ?>
+<body>
+	<div class=" container ">
+		<h1>Insert a new message in the public board</h1>
+		<div class=" row">
+			<div class=" span6 ">
+				<h5>Simple form (post-validated) with batched errors (no standard HTML)</h5>
+				<p>
+ 					<?php echo $errorMessageSimpleForm ; ?>
+ 					<?php include_once 'basic_message_form.php'; ?>
+ 				</p>
+			</div>
+			<div class=" span6 ">
+				<h5>Field-by-field interactive form (post-validated) with batched errors (standard HTML5)</h5>
+				<p>
+ 					<?php echo $errorMessageInteractiveForm; ?>
+ 					<?php include_once 'interactive_message_form.php'; ?>
+ 				</p>
+			</div>
+		</div>
+		<hr>
+		<h1>Public board</h1>
+ 			<?php echo $messageList; ?>
+	</div>
+
+</body>
 </html>
