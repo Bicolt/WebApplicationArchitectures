3c9bf4cc02b091c6a0c5696e97d9b076841cc20a
Nicolas Benning <nbenning@free.fr>
2014-11-03 16:44:51 +0000

    doctype head template

--- Board/app/templates/html_doctype_and_head.php
+++ Board/app/templates/html_doctype_and_head.php
@@ -1,18 +1,13 @@
-<!doctype html>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<meta name="description" content="template MVC">
-		<meta name="author" content="Nicolas Benning">
-		<link href="css/bootstrap/css/bootstrap.css" rel="stylesheet">
-		<title>Publications</title>
-	</head>
+<head>
+	<meta charset="utf -8">
+	<title> <?php echo $appTitle; ?> </title>
+	<meta name="description" content=" &lt;?php echo $appTitle; ?&gt;">
+	<meta name="author" content="Nicolas Benning">
 	
-	<body>
-			<div class='container'>
-					<h1>Publications</h1>
-					<p><?php echo $publications_box ?></p>
-					<p><a class='btn btn-primary' href='index.php?HomePageClick'>Home &raquo</a></p>
-			</div>
-	</body>
-</html>
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<style type="text/css">
+		body {
+			padding-top: 30px;
+		}
+	</style>
+</head>
