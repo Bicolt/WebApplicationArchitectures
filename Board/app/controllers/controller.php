0cde2b91ce923cae7d9bbc9a486768791c7c8010
Nicolas Benning <nbenning@free.fr>
2014-11-03 16:19:58 +0000

    Controller

--- Board/app/controllers/controller.php
+++ Board/app/controllers/controller.php
@@ -1,18 +1,39 @@
 <?php
 
 /** @author Nicolas Benning
- * @example MVC
  */
-
 class Controller {
-	
 	private $model;
-	
-	public function __construct($model) {
+	public function __construct($model, $action = null, $parameters) {
 		$this->model = $model;
+		if ($action != null) {
+			switch ($action) {
+				case " insertNewMessageFromSimpleForm " :
+					$this - insertNewMessageParameters ( $parameters, MESSAGE_FORM_SIMPLE );
+					break;
+				case " insertNewMessageFromInteractiveForm " :
+					$this - insertNewMessageParameters ( $parameters, MESSAGE_FORM_INTERACTIVE );
+					break;
+				default :
+			}
+		}
+		$this->model->prepareMessagesList ();
 	}
-	
-	public function clicked() {
-		$this->model->str = "Well done!";
+	/**
+	 *
+	 * @param : $parameters
+	 *        	- array containing the parameters to be validated
+	 */
+	public function insertNewMessageParameters($parameters, $formType) {
+		$authorEmail = $parameters [" fAuthorEmail "];
+		$title = $parameters [" fMessageTitle "];
+		$content = $parameters [" fMessageContent "];
+		$areParsValid = $this->model->areMessageFormParametersValid ( $authorEmail, $title, $content );
+		if ($areParsValid)
+			$this->insertNewMessage ( $authorEmail, $title, $content );
+		else
+			$this->model->setUpErrorMessage ( $formType );
 	}
 }
+
+?>
