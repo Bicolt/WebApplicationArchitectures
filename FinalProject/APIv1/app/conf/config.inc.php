f629b5a0fe5a600b4b26665d6c92655cde0daa02
Nicolas Benning <nicolas.benning@mydit.ie>
2014-12-30 02:10:27 +0100

    update config

--- FinalProject/APIv1/app/conf/config.inc.php
+++ FinalProject/APIv1/app/conf/config.inc.php
@@ -1,6 +1,6 @@
 <?php
-define("DB_HOST", "REMOTE-MYSQL3.SERVAGE.NET");
+define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
-define("DB_NAME", "C");
+define("DB_NAME", "D14122954");
 ?>
